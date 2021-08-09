2722b61057507ec1e49197dcf8ce9fc1b33f4ea2
Lindenbar <bor.tim.1997@gmail.com>
2021-08-09 19:12:47 +0300

    Added addressStyle.css include

--- public_html/protected/php/includeStyles.php
+++ public_html/protected/php/includeStyles.php
@@ -1,3 +1,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Jura&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/headerStyle.css">
+<link rel="stylesheet" type="text/css" href="css/addressStyle.css">
