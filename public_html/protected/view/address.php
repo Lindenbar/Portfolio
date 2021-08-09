8b3da513b525f2197d0d0c8c4565aa7237bc118a
Lindenbar <bor.tim.1997@gmail.com>
2021-08-09 19:05:23 +0300

    Add address section

---
+++ public_html/protected/view/address.php
@@ -0,0 +1,9 @@
+<section id="address" class="infoBlock">
+    <div class="blockContent">
+        <ul>
+            <li><a href="#">address</a></li>
+            <li><a href="#">email</a></li>
+            <li><a href="#">website</a></li>
+        </ul>
+    </div>
+</section>
