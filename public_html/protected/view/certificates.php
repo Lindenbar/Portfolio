626ec1d32a4d3d7c3837ffef01e514bacd4bf78d
Lindenbar <bor.tim.1997@gmail.com>
2021-08-14 06:17:47 +0300

    Added second certificate

--- public_html/protected/view/certificates.php
+++ public_html/protected/view/certificates.php
@@ -19,5 +19,23 @@
                 </div>
             </div>
         </div>
+        <div class="certificate">
+            <div class="certificateTitle">Веб-разработка для начинающих: HTML и CSS</div>
+            <div class="certificateBox">
+                <img src="img/sertificates/web-development-for-beginners-html-css.png" alt="Сертификат"/>
+                <div class="certificateAction">
+                    <a href="https://stepik.org/cert/1054309">
+                        <div title="Источник">
+                            <?php include 'img/svg/link-icon.php'?>
+                        </div>
+                    </a>
+                    <a href="files/certificates/web-development-for-beginners-html-css.pdf">
+                        <div title="Скачать">
+                            <?php include 'img/svg/download-icon.php'?>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
